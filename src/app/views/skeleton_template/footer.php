72e27a2dc6a8894f56b2883c1e7a9c3e9474400f
Mukul Hase <mukulhase@gmail.com>
2016-12-22 03:49:28 +0530

    Removed hardcoded button positions

--- src/app/views/skeleton_template/footer.php
+++ src/app/views/skeleton_template/footer.php
@@ -11,15 +11,25 @@ if (empty($is_ajax)):
         <div class="y"></div>
     </div>
 </button>
-<button class="toggle-contact fb-btn" onclick="window.open('https://www.facebook.com/felicity.iiith/')">
-  <i class="icon-facebook"></i>
-</button>
-<button class="toggle-contact youtube-btn" onclick="window.open('https://www.youtube.com/channel/UC_1vMv4Al_96QgYzkFjh99w/')">
-  <i class="icon-youtube"></i>
-</button>
-<button class="toggle-contact instagram-btn" onclick="window.open('https://www.instagram.com/felicity.iiith/')">
-  <i class="icon-instagram"></i>
-</button>
+<div style="width: 100%" class="btn-box">
+<table style="margin: 0 auto">
+<tr>
+<td>
+  <button class="toggle-contact fb-btn" onclick="window.open('https://www.facebook.com/felicity.iiith/')">
+    <i class="icon-facebook"></i>
+  </button>
+  </td><td>
+  <button class="toggle-contact youtube-btn" onclick="window.open('https://www.youtube.com/channel/UC_1vMv4Al_96QgYzkFjh99w/')">
+    <i class="icon-youtube"></i>
+  </button>
+  </td><td>
+  <button class="toggle-contact instagram-btn" onclick="window.open('https://www.instagram.com/felicity.iiith/')">
+    <i class="icon-instagram"></i>
+  </button>
+  </td>
+  </tr>
+</table>
+</div>
 <!-- Scripts -->
 <script src="<?= base_url() ?>static/scripts/common.js" charset="utf-8"></script>
 <script src="<?= base_url() ?>static/scripts/ajaxify.js" charset="utf-8"></script>
