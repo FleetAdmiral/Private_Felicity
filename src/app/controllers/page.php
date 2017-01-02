82dfb61880cb52a6d949bb404348ff8be5ac4db2
Mukul Hase <mukulhase@gmail.com>
2017-01-03 00:13:58 +0530

    nginx fix

--- src/app/controllers/page.php
+++ src/app/controllers/page.php
@@ -1,5 +1,20 @@
 <?php
+if (!function_exists('getallheaders'))  {
+    function getallheaders()
+    {
+        if (!is_array($_SERVER)) {
+            return array();
+        }
 
+        $headers = array();
+        foreach ($_SERVER as $name => $value) {
+            if (substr($name, 0, 5) == 'HTTP_') {
+                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
+        }
+        return $headers;
+    }
+}
 class page extends Controller {
 
     function __construct() {
