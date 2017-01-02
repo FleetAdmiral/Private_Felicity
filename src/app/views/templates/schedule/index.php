afc5e399cb72ab752d6ac2f8011770509d9c3aa3
Anshul Singhal <sganshuliiith@gmail.com>
2017-01-02 14:49:58 +0530

    [schedule] Fixes alignement issue and Feb calender is removed from code

--- src/app/views/templates/schedule/index.php
+++ src/app/views/templates/schedule/index.php
@@ -110,67 +110,6 @@
                 </tbody>
             </table>
         </div>
-        <div class="cal-month">
-            <table class="cal-table" data-month="Feb">
-                <thead>
-                    <tr>
-                        <th class="cal-month-name" colspan="7"><?= strftime('%B %Y', strtotime('February 2017')) ?></th>
-                    </tr>
-                    <tr>
-                        <th><?= strftime('%a', strtotime('Sun')) ?></th>
-                        <th><?= strftime('%a', strtotime('Mon')) ?></th>
-                        <th><?= strftime('%a', strtotime('Tue')) ?></th>
-                        <th><?= strftime('%a', strtotime('Wed')) ?></th>
-                        <th><?= strftime('%a', strtotime('Thu')) ?></th>
-                        <th><?= strftime('%a', strtotime('Fri')) ?></th>
-                        <th><?= strftime('%a', strtotime('Sat')) ?></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td class="empty"></td>
-                        <td class="empty"></td>
-                        <td class="empty"></td>
-                        <td<?php $is_imp('2-1'); ?>>1</td>
-                        <td<?php $is_imp('2-2'); ?>>2</td>
-                        <td<?php $is_imp('2-3'); ?>>3</td>
-                        <td<?php $is_imp('2-4'); ?>>4</td>
-                    </tr>
-                    <tr>
-                        <td<?php $is_imp('2-5'); ?>>5</td>
-                        <td<?php $is_imp('2-6'); ?>>6</td>
-                        <td<?php $is_imp('2-7'); ?>>7</td>
-                        <td<?php $is_imp('2-8'); ?>>8</td>
-                        <td<?php $is_imp('2-9'); ?>>9</td>
-                        <td<?php $is_imp('2-10'); ?>>10</td>
-                        <td<?php $is_imp('2-11'); ?>>11</td>
-                    </tr>
-                    <tr>
-                        <td<?php $is_imp('2-12'); ?>>12</td>
-                        <td<?php $is_imp('2-13'); ?>>13</td>
-                        <td<?php $is_imp('2-14'); ?>>14</td>
-                        <td<?php $is_imp('2-15'); ?>>15</td>
-                        <td<?php $is_imp('2-16'); ?>>16</td>
-                        <td<?php $is_imp('2-17'); ?>>17</td>
-                        <td<?php $is_imp('2-18'); ?>>18</td>
-                    </tr>
-                    <tr>
-                        <td<?php $is_imp('2-19'); ?>>19</td>
-                        <td<?php $is_imp('2-20'); ?>>20</td>
-                        <td<?php $is_imp('2-21'); ?>>21</td>
-                        <td<?php $is_imp('2-22'); ?>>22</td>
-                        <td<?php $is_imp('2-23'); ?>>23</td>
-                        <td<?php $is_imp('2-24'); ?>>24</td>
-                        <td<?php $is_imp('2-25'); ?>>25</td>
-                    </tr>
-                    <tr>
-                        <td<?php $is_imp('2-26'); ?>>26</td>
-                        <td<?php $is_imp('2-27'); ?>>27</td>
-                        <td<?php $is_imp('2-28'); ?>>28</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
     </div>
     <div class="col6">
         <table class="eventslist">
