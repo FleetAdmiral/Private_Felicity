5c26fcfdd83f3a5528dbc7d49b675e3b31db734d
Anshul Singhal <sganshuliiith@gmail.com>
2017-01-20 11:24:47 +0530

    Kaizala chut pat increased

--- src/app/views/contest/kaizala.php
+++ src/app/views/contest/kaizala.php
@@ -7,6 +7,6 @@
         <p>Create your proposals in the format as displayed in the <a href="http://felicity.iiit.ac.in/files/17/Kaizala_template.docx" target="_blank"> template.</a></p>
         <p>A <a href="http://felicity.iiit.ac.in/files/17/Kaizala_Sample.docx" target="_blank"> Sample Proposal </a> can be found here.</p>
         <p>Mail this proposal to <a href="mailto:kaizala_hackathon@microsoft.com"> kaizala_hackathon@microsoft.com </a></p>
-        <p>Submission deadline: 10 am on Tuesday, 24 Jan 2017 </p>
+        <p>Techincal introduction of Developing on Kaizala can be found <a href="https://felicity.iiit.ac.in/files/17/kaizala_intro.pdf" target="_blank">here</a>.</p>
     </div>
 </article>
