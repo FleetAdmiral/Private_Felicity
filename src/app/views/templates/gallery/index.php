0eb6ac409f186f041a12b18cebbcbcea77f881f8
Anubhab Sen <anubhabsen@gmail.com>
2017-12-07 22:18:30 +0530

    new images in gallery

--- src/app/views/templates/gallery/index.php
+++ src/app/views/templates/gallery/index.php
@@ -2,7 +2,7 @@
 <div style="zoom:0.5; height: 100%">
     <section id="photostack-1" class="photostack">
     <div id="inner">
-      
+
     </div>
   </section>
   </div>
@@ -14,13 +14,11 @@
   <script>
     var stack = document.getElementById('photostack-1')
     var galleryImages = [
-          "6.jpg","HairPainting.jpg","old_87.jpg","old_85.jpg","old_84.jpg",
-          "old_83.jpg","old_79.jpg","old_74.jpg","old_55.jpg","old_53.jpg",
-          "old_41.jpg","old_40.jpg","old_39.jpg","old_38.jpg","old_35.jpg",
-          "old_34.jpg","old_30.jpg","IMG_2286.jpg","IMG_0263.jpg","IMG_0227.jpg",
-          "IMG_0132.jpg","9.jpg","8.jpg","7.jpg","5.jpg","4.jpg","old_88.jpg",
-          "3.jpg","22.jpg","21.jpg","20.jpg","2.jpg","19.jpg","18.jpg","17.jpg",
-          "16.jpg","15.jpg","14.jpg","13.jpg","12.jpg","11.jpg","10.jpg","1.jpg"
+          "1.jpg","2.jpg","3.jpg","4.jpg","5.jpg",
+          "6.jpg","7.jpg","8.jpg","9.jpg","10.jpg",
+          "11.jpg","12.jpg","13.jpg","14.jpg","15.jpg",
+          "16.jpg","17.jpg","18.jpg","19.jpg","20.jpg",
+          "21.jpg","22.jpg","23.jpg","24.jpg","25.jpg","26.jpg","27.jpg"
       ]
     var getImage = function(imageName) {
           var F = document.createElement("figure");
@@ -39,7 +37,7 @@
      photostack = new Photostack(stack);
      setTimeout(function(){photostack._resizeHandler();},100);
      function changePhoto(dir){
-        
+
         if(dir==37){
           if (photostack.current == 0){
           photostack._showPhoto(photostack.allItemsCount-1);
