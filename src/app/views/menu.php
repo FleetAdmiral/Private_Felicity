54a53162230f42f6cd71cc820238da7257b5b27f
Anubhab Sen <anubhabsen@gmail.com>
2018-01-04 02:01:47 +0530

    team redirects to contact

--- src/app/views/menu.php
+++ src/app/views/menu.php
@@ -3,7 +3,7 @@
          <a onclick="toggleDetails('about')" style="font-size: 150%; color: white;"><span>About</span></a>
          <a onclick="toggleDetails('events')" style="font-size: 150%; color: white; padding-left: 6%;"><span>Events</span></a>
          <a onclick="toggleDetails('gallery')" style="font-size: 150%; color: white; padding-left: 6%;"><span>Gallery</span></a>
-         <a onclick="toggleDetails('team')" style="font-size: 150%; color: white; padding-left: 6%;"><span>Team</span></a>
+         <a onclick="toggleDetails('contact')" style="font-size: 150%; color: white; padding-left: 6%;"><span>Team</span></a>
          <a target="_blank" href="/useproperpathhereplease/diaries/main.html" style="font-size: 150%; color: white; padding-left: 6%;"><span>Felicity Diaries</span></a>
          <a onclick="toggleDetails('sponsors')" style="font-size: 150%; color: white; padding-left: 6%;"><span>Sponsors</span></a>
          <a onclick="toggleDetails('contact')" style="font-size: 150%; color: white; padding-left: 6%;"><span>Contact</span></a>
