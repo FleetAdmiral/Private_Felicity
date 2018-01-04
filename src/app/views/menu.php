4ea21bada9b4bfc1c88a8b8245f7addedf0a27f4
Megh Parikh <meghprkh@gmail.com>
2018-01-05 00:34:26 +0530

    Fix felicity diaries

--- src/app/views/menu.php
+++ src/app/views/menu.php
@@ -4,7 +4,7 @@
          <a onclick="toggleDetails('events')" style="font-size: 150%; color: white; padding-left: 6%;"><span>Events</span></a>
          <a onclick="toggleDetails('gallery')" style="font-size: 150%; color: white; padding-left: 6%;"><span>Gallery</span></a>
          <!-- <a onclick="toggleDetails('contact')" style="font-size: 150%; color: white; padding-left: 6%;"><span>Team</span></a> -->
-         <a target="_blank" href="/useproperpathhereplease/diaries/main.html" style="font-size: 150%; color: white; padding-left: 6%;"><span>Felicity Diaries</span></a>
+         <a target="_blank" href="/files/18/diaries/main.html" style="font-size: 150%; color: white; padding-left: 6%;"><span>Felicity Diaries</span></a>
          <a onclick="toggleDetails('sponsors')" style="font-size: 150%; color: white; padding-left: 6%;"><span>Sponsors</span></a>
          <a onclick="toggleDetails('contact')" style="font-size: 150%; color: white; padding-left: 6%;"><span>Contact</span></a>
    </div>
