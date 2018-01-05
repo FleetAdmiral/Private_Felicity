b965eba2662af6d6064c18285be6cbe9e7b62511
Anubhab Sen <anubhabsen@gmail.com>
2018-01-05 23:16:28 +0530

    adds sap link

--- src/app/views/templates/team/index.php
+++ src/app/views/templates/team/index.php
@@ -302,6 +302,9 @@ function img($img_name, $name=null) {
         <!-- </div> -->
     <!-- </div> -->
 <!-- </div> -->
+<div style="padding-bottom: 1em;">
+    <a href="felicity.iiit.ac.in/sap"><h2 class="text-center">Join Our Team Here</h2></a>
+</div>
 </article>
 <?php $this->load_fragment('skeleton_template/footer'); ?>
 <?php if (!$is_ajax): ?>
