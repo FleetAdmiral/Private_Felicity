a50f58a9195b9bebf30678f6aa102bde7d0813f3
Mukul Hase <mukulhase@gmail.com>
2018-01-08 20:16:44 +0000

    Slight cleanup

--- src/app/views/menu.php
+++ src/app/views/menu.php
@@ -1,11 +1,11 @@
 <div style="width: 100%" class="btn-box" style="z-index: 4">
   <div style="text-align: center;">
-         <a onclick="toggleDetails('about')" class="menutext" style="font-size: 150%; color: white;"><span>About</span></a>
-         <a onclick="toggleDetails('events')" class="menutext" style="font-size: 150%; color: white; padding-left: 6%;"><span>Events</span></a>
-         <a onclick="toggleDetails('gallery')" class="menutext" style="font-size: 150%; color: white; padding-left: 6%;"><span>Gallery</span></a>
-         <!-- <a onclick="toggleDetails('contact')" class="menutext" style="font-size: 150%; color: white; padding-left: 6%;"><span>Team</span></a> -->
-         <a target="_blank" href="/files/18/diaries" class="menutext" style="font-size: 150%; color: white; padding-left: 6%;"><span>Felicity Diaries</span></a>
-         <a onclick="toggleDetails('sponsors')" class="menutext" style="font-size: 150%; color: white; padding-left: 6%;"><span>Sponsors</span></a>
-         <a onclick="toggleDetails('contact')" class="menutext" style="font-size: 150%; color: white; padding-left: 6%;"><span>Contact</span></a>
+         <a onclick="toggleDetails('about')" class="menutext" style=""><span>About</span></a>
+         <a onclick="toggleDetails('events')" class="menutext" style=" padding-left: 6%;"><span>Events</span></a>
+         <a onclick="toggleDetails('gallery')" class="menutext" style=" padding-left: 6%;"><span>Gallery</span></a>
+         <!-- <a onclick="toggleDetails('contact')" class="menutext" style=" padding-left: 6%;"><span>Team</span></a> -->
+         <a target="_blank" href="/files/18/diaries" class="menutext" style=" padding-left: 6%;"><span>Felicity Diaries</span></a>
+         <a onclick="toggleDetails('sponsors')" class="menutext" style=" padding-left: 6%;"><span>Sponsors</span></a>
+         <a onclick="toggleDetails('contact')" class="menutext" style=" padding-left: 6%;"><span>Contact</span></a>
    </div>
 </div>
