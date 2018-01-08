60c96649cfe2c664938379ede3b28da6dff58eab
Mukul Hase <mukulhase@gmail.com>
2018-01-08 20:12:11 +0000

    Globe and clouds and glow

--- src/app/views/menu.php
+++ src/app/views/menu.php
@@ -1,11 +1,11 @@
 <div style="width: 100%" class="btn-box" style="z-index: 4">
   <div style="text-align: center;">
-         <a onclick="toggleDetails('about')" style="font-size: 150%; color: white;"><span>About</span></a>
-         <a onclick="toggleDetails('events')" style="font-size: 150%; color: white; padding-left: 6%;"><span>Events</span></a>
-         <a onclick="toggleDetails('gallery')" style="font-size: 150%; color: white; padding-left: 6%;"><span>Gallery</span></a>
-         <!-- <a onclick="toggleDetails('contact')" style="font-size: 150%; color: white; padding-left: 6%;"><span>Team</span></a> -->
-         <a target="_blank" href="/files/18/diaries" style="font-size: 150%; color: white; padding-left: 6%;"><span>Felicity Diaries</span></a>
-         <a onclick="toggleDetails('sponsors')" style="font-size: 150%; color: white; padding-left: 6%;"><span>Sponsors</span></a>
-         <a onclick="toggleDetails('contact')" style="font-size: 150%; color: white; padding-left: 6%;"><span>Contact</span></a>
+         <a onclick="toggleDetails('about')" class="menutext" style="font-size: 150%; color: white;"><span>About</span></a>
+         <a onclick="toggleDetails('events')" class="menutext" style="font-size: 150%; color: white; padding-left: 6%;"><span>Events</span></a>
+         <a onclick="toggleDetails('gallery')" class="menutext" style="font-size: 150%; color: white; padding-left: 6%;"><span>Gallery</span></a>
+         <!-- <a onclick="toggleDetails('contact')" class="menutext" style="font-size: 150%; color: white; padding-left: 6%;"><span>Team</span></a> -->
+         <a target="_blank" href="/files/18/diaries" class="menutext" style="font-size: 150%; color: white; padding-left: 6%;"><span>Felicity Diaries</span></a>
+         <a onclick="toggleDetails('sponsors')" class="menutext" style="font-size: 150%; color: white; padding-left: 6%;"><span>Sponsors</span></a>
+         <a onclick="toggleDetails('contact')" class="menutext" style="font-size: 150%; color: white; padding-left: 6%;"><span>Contact</span></a>
    </div>
 </div>
--- src/app/views/skeleton_template/footer.php
+++ src/app/views/skeleton_template/footer.php
@@ -69,7 +69,37 @@ if (empty($is_ajax)):
         display: block;
       }
     </style>
-    <img src= "<?= base_url() ?>static/images/world.png" class="globe" style="z-index:2; position:absolute; left:25vw; width: 50%; height: auto; bottom: -30vw">
+    <div class="cloud-parent" style="z-index:2;">
+      <?php
+      for ($i = 1; $i <= 15; $i++) {
+        ?>
+      <div style="transform: rotate( <?= rand(1,360) ?>deg);">
+        <div class="cloud globe" style="
+          position:absolute;
+          animation-duration: <?=35+rand(-5,5)?>s !important;
+          top: 100%;
+          left: 50%;
+          ">
+          <img src= "<?= base_url() ?>static/images/cloud<?=rand(1,3)?>.png" class="cloud-img" style="
+          position: relative;
+          z-index:2;
+          transform: translate(-50%, -50%);
+          bottom:<?=28+ rand(-5,5) ?>vw;
+          width:9vw; height: auto;">
+        </div>
+      </div>
+      <?php
+      }
+      ?>
+    </div>
+    <img src= "<?= base_url() ?>static/images/world.png" class="globe" style="
+    z-index:2;
+    position:absolute;
+    left:50%;
+    width: 50vw;
+    height: auto;
+    top:100%;
+    transform: translate(-50%, -50%);">
     <?php $this->load_fragment('menu'); ?>
 <?php $this->load_fragment('google_analytics'); ?>
     </body>
