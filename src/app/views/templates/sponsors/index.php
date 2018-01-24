123c73511f0e4c7062dacc488736237c0d6c87bc
Pranav Nair <pranavnair97@gmail.com>
2018-01-24 21:30:18 +0000

    Added new sponsor logos

--- src/app/views/templates/sponsors/index.php
+++ src/app/views/templates/sponsors/index.php
@@ -40,6 +40,311 @@ $sponsors2014 = [
 ];
 ?>
 <article class="page sponsors">
+  <div class="container">
+      <!--<header>
+          <h1>Spon<span class="tabheading">sors</span></h1>
+      </header>-->
+      <h2><?= __('Felicity 2018 Sponsors') ?></h2>
+      <div class="row some-top-margin">
+          <div class="col6 offset3">
+              <p>Co-Sponsor</p>
+              <img src="<?= base_url() ?>static/images/sponsors/18/CDK_Global.jpg" alt="CDK Global"/>
+          </div>
+      </div>
+      <div class="row some-top-margin">
+        <div class="col4 container">
+            <p>Associate Sponsor</p>
+                <img src="<?= base_url() ?>static/images/sponsors/18/telangana_tourism.jpg" alt="Telangana Tourism"/>
+            </div>
+          <div class="col4 container">
+              <p>Powered By</p>
+              <img src="<?= base_url() ?>static/images/sponsors/18/sbi.png" alt="SBI"/>
+          </div>
+          <div class="col4 container">
+              <p>Kings of ML</p>
+              <img src="<?= base_url() ?>static/images/sponsors/18/DE_SHAW.jpg" alt="DE Shaw"/>
+          </div>
+        </div>
+      <div class="row some-top-margin">
+        <div class="col4 container">
+            <p>CodeCraft Sponsor</p>
+            <img src="<?= base_url() ?>static/images/sponsors/18/caTech.jpg" alt="CA Technologies"/>
+        </div>
+        <div class="col4 container">
+            <p>Human-like AI sponsor</p>
+            <img src="<?= base_url() ?>static/images/sponsors/17/microsoft.jpg" alt="Microsoft"/>
+        </div>
+        <div class="col4 container">
+            <p>Life Insurance Sponsor</p>
+            <img src="<?= base_url() ?>static/images/sponsors/18/LIC.png" alt="LIC"/>
+        </div>
+      </div>
+      <div class="row some-top-margin">
+        <div class="col4 container">
+            <p>Food Delivery Partner</p>
+            <img src="<?= base_url() ?>static/images/sponsors/18/Zomato.png" alt="Zomato"/>
+        </div>
+        <div class="col4 container">
+            <p>Telecom Partner</p>
+            <img src="<?= base_url() ?>static/images/sponsors/18/airtel.png" alt="Airtel logo"/>
+        </div>
+        <div class="col4 container">
+            <p>News Channel Partner</p>
+            <img src="<?= base_url() ?>static/images/sponsors/18/EENADU.png" alt="Eenadu"/>
+        </div>
+      </div>
+      <div class="row some-top-margin">
+        <div class="col4 container">
+            <p>Audio Partner</p>
+            <img src="<?= base_url() ?>static/images/sponsors/18/zebronics.png" alt="Zebronics Logo"/>
+        </div>
+        <div class="col4 container">
+            <p>Music Channel Partner</p>
+            <img src="<?= base_url() ?>static/images/sponsors/18/9XM_red_logo.jpg" alt="9xM"/>
+        </div>
+        <div class="col4 container">
+            <p>Supply Chain Partner</p>
+            <img src="<?= base_url() ?>static/images/sponsors/18/safexprs.jpg" alt="Safe Express"/>
+        </div>
+      </div>
+      <div class="row some-top-margin">
+        <div class="col4 container">
+            <p>Festpav</p>
+            <img src="<?= base_url() ?>static/images/sponsors/18/festPav.png" alt="Festpav Logo"/>
+        </div>
+        <div class="col4 container">
+            <p>Storm the Yard sponsor</p>
+            <img src="<?= base_url() ?>static/images/sponsors/18/soundkraft.png" alt="Soundkraft"/>
+        </div>
+        <div class="col4 container">
+            <p>Gaming Partner</p>
+            <img src="<?= base_url() ?>static/images/sponsors/18/playmax.png" alt="Gaming Partner"/>
+        </div>
+  <!--        <div class="col4 container">
+              <p>Entertainment Partner</p>
+              <img src="<?= base_url() ?>static/images/sponsors/18/entertainment.png" alt="White Heaven Logo"/>
+          </div>-->
+      </div>
+      <div class="row some-top-margin">
+        <div class="col4 container">
+            <p>Nights Sponsor</p>
+            <img src="<?= base_url() ?>static/images/sponsors/18/pehchan.jpg" alt="Pechan Music"/>
+        </div>
+        <div class="col4 container">
+            <p>Nights Sponsor</p>
+            <img src="<?= base_url() ?>static/images/sponsors/18/NCM_Final.jpg" alt="NCM"/>
+        </div>
+        <div class="col4 container">
+            <p>Entertainment Partner</p>
+            <img src="<?= base_url() ?>static/images/sponsors/18/jrny.png" alt="Journey Entertainment"/>
+        </div>
+      </div>
+      <div class="row some-top-margin">
+      </div>
+      <h2><?= __('Felicity 2017 Sponsors') ?></h2>
+      <div class="row some-top-margin">
+          <div class="col6 offset3">
+              <p>Title Sponsor</p>
+              <img src="<?= base_url() ?>static/images/sponsors/17/qualcomm.jpg" alt="Qualcomm logo"/>
+          </div>
+      </div>
+      <p>Powered By</p>
+      <div class="row some-top-margin">
+          <div class="col3 offset3 container">
+              <img src="<?= base_url() ?>static/images/sponsors/17/9xo.jpg" alt="9XO Logo"/>
+          </div>
+          <div class="col3 container">
+              <img src="<?= base_url() ?>static/images/sponsors/17/9xm.jpg" alt="9XM Logo"/>
+          </div>
+      </div>
+      <div class="row some-top-margin">
+          <div class="col4 container">
+              <p>Associate Sponsor</p>
+              <img src="<?= base_url() ?>static/images/sponsors/17/sbh.png" alt="SBH logo"/>
+          </div>
+          <div class="col4 container">
+              <p><a class="underlined" href="<?= base_url() ?>threads/">Threads</a> sponsor</p>
+              <img src="<?= base_url() ?>static/images/sponsors/17/microsoft.jpg" alt="Microsoft Logo"/>
+          </div>
+          <div class="col4 container">
+              <p>Telecom Partner</p>
+              <img src="<?= base_url() ?>static/images/sponsors/17/airtel.jpg" alt="Airtel logo"/>
+          </div>
+      </div>
+      <div class="row some-top-margin">
+          <div class="col4 container">
+              <p>Codecraft and Kings of ML</p>
+              <img src="<?= base_url() ?>static/images/sponsors/17/progress.png" alt="Progress Logo"/>
+          </div>
+          <div class="col4 container">
+              <p>Health Partner</p>
+              <img src="<?= base_url() ?>static/images/sponsors/17/reddy.jpg" alt="Dr Reddy Logo"/>
+          </div>
+          <div class="col4 container">
+              <p>Audio Partner</p>
+              <img src="<?= base_url() ?>static/images/sponsors/17/zebronics.png" alt="Zebronics Logo"/>
+          </div>
+      </div>
+      <div class="row some-top-margin">
+          <div class="col4 container">
+              <p>Radio Partner</p>
+              <img src="<?= base_url() ?>static/images/sponsors/17/redfm.png" alt="RedFM Logo"/>
+          </div>
+          <div class="col4 container">
+              <p>Travel Partner</p>
+              <img src="<?= base_url() ?>static/images/sponsors/17/uber.png" alt="Uber Logo"/>
+          </div>
+          <div class="col4 container">
+              <p>Dare Partner</p>
+              <img src="<?= base_url() ?>static/images/sponsors/17/dare.png" alt="Dare Logo"/>
+          </div>
+      </div>
+      <div class="row some-top-margin">
+          <div class="col4 container">
+              <p>Merchandise Partner</p>
+              <img src="<?= base_url() ?>static/images/sponsors/17/merchandise.png" alt="Your Merchandise Logo"/>
+          </div>
+          <div class="col4 container">
+              <p>Cafe Partner</p>
+              <img src="<?= base_url() ?>static/images/sponsors/17/cafe.png" alt="Barista Logo"/>
+          </div>
+          <div class="col4 container">
+              <p>Gaming Partner</p>
+              <img src="<?= base_url() ?>static/images/sponsors/17/blaze.jpg" alt="Blaze Logo"/>
+          </div>
+      </div>
+      <div class="row some-top-margin">
+          <div class="col4 container">
+              <p>Riders on Storm Title Sponsor</p>
+              <img src="<?= base_url() ?>static/images/sponsors/17/musicneedsyou.jpg" alt="Music needs you Logo"/>
+          </div>
+          <div class="col4 container">
+              <p>Riders on Storm Associate Sponsor</p>
+              <img src="<?= base_url() ?>static/images/sponsors/17/soundkraft.jpg" alt="SoundKraft Logo"/>
+          </div>
+          <div class="col4 container">
+              <p>Official Ticket Partner</p>
+              <img src="<?= base_url() ?>static/images/sponsors/17/bms.png" alt="Book My show Logo"/>
+          </div>
+      </div>
+      <div class="row some-top-margin">
+          <div class="col4 container">
+              <p>Entertainment Partner</p>
+              <img src="<?= base_url() ?>static/images/sponsors/17/entertainment.png" alt="White Heaven Logo"/>
+          </div>
+          <div class="col4 container">
+              <p>Online Outreach Partner</p>
+              <img src="<?= base_url() ?>static/images/sponsors/17/justapplause.png" alt="Just Applause Logo"/>
+          </div>
+          <div class="col4 container">
+              <p>Adventure Partner</p>
+              <img src="<?= base_url() ?>static/images/sponsors/17/exploreout.png" alt="Explore Out Logo"/>
+          </div>
+      </div>
+      <h2><?= __('Past sponsors') ?></h2>
+      <h3><?= __('Year 2016') ?></h3>
+      <div class="row some-top-margin">
+          <div class="col6 offset3">
+              <p>Title Sponsor</p>
+              <img src="<?= base_url() ?>static/images/sponsors/qualcomm.jpg" alt="Qualcomm logo"/>
+          </div>
+      </div>
+      <p>Powered By</p>
+      <div class="row some-top-margin">
+          <div class="col3 offset3 container">
+              <img src="<?= base_url() ?>static/images/sponsors/9xo.jpg" alt="9XO Logo"/>
+          </div>
+          <div class="col3 container">
+              <img src="<?= base_url() ?>static/images/sponsors/9xm.png" alt="9XM Logo"/>
+          </div>
+      </div>
+      <div class="row some-top-margin">
+          <div class="col4 container">
+              <p><a class="underlined" href="<?= base_url() ?>threads/kings-of-ml/">Kings of ML</a> and <a class="underlined" href="<?= base_url() ?>threads/botomata/">Botomata</a> sponsor</p>
+              <img src="<?= base_url() ?>static/images/sponsors/microsoft.png" alt="Microsoft Logo"/>
+          </div>
+          <div class="col4 container">
+              <p>Nights Sponsor</p>
+              <img src="<?= base_url() ?>static/images/sponsors/indeed.png" alt="Indeed Logo"/>
+          </div>
+          <div class="col4 container">
+              <p>Official Banking Partner</p>
+              <img src="<?= base_url() ?>static/images/sponsors/sbh.png" alt="SBH Logo"/>
+          </div>
+      </div>
+      <div class="row some-top-margin">
+          <div class="col4 container">
+              <p>Digital Media Partner</p>
+              <img src="<?= base_url() ?>static/images/sponsors/chaibisket.png" alt="Chai Bisket Logo"/>
+          </div>
+          <div class="col4 container">
+              <p>Official Coupons Partner</p>
+              <img src="<?= base_url() ?>static/images/sponsors/nearby.png" alt="Nearby Logo"/>
+          </div>
+          <div class="col4 container">
+              <p>Progress Software</p>
+              <img src="<?= base_url() ?>static/images/sponsors/progress.jpg" alt="Progress Logo"/>
+          </div>
+      </div>
+      <div class="row some-top-margin">
+          <div class="col4 offset4 container">
+              <p>Café Partners</p>
+              <img src="<?= base_url() ?>static/images/sponsors/ccd.png" alt="CCD Logo"/>
+          </div>
+      </div>
+      <?php foreach (array_chunk($sponsors2016, 4) as $sponsors): ?>
+          <div class="row some-top-margin">
+              <?php foreach ($sponsors as $sponsor): ?>
+                  <div class="col3 container">
+                      <p><?= $sponsor['name'] ?></p>
+                      <img src="<?= base_url() .'static/images/sponsors/'. $sponsor['image'] ?>" alt="<?= $sponsor['name'] ?> Logo" />
+                  </div>
+              <?php endforeach; ?>
+          </div>
+      <?php endforeach; ?>
+      <h3><?= __('Year 2015') ?></h3>
+      <div class="row some-top-margin">
+          <div class="col6 offset3">
+              <img src="<?= base_url() ?>static/images/sponsors/qualcomm.jpg" alt="Qualcomm logo"/>
+              <p>Qualcomm (Title Sponsor)</p>
+          </div>
+      </div>
+      <div class="row some-top-margin">
+          <div class="col4 container">
+              <img src="<?= base_url() ?>static/images/sponsors/microsoft.png" alt="Microsoft Logo"/>
+              <p>Microsoft</p>
+          </div>
+          <div class="col4 container">
+              <img src="<?= base_url() ?>static/images/sponsors/acacian.png" alt="Acacia Logo"/>
+              <p>Acacia</p>
+          </div>
+          <div class="col4 container">
+              <img src="<?= base_url() ?>static/images/sponsors/fantasian.png" alt="Fantasian Logo"/>
+              <p>Fantasian (Official Gaming Partner)</p>
+          </div>
+      </div>
+      <?php foreach (array_chunk($sponsors2015, 4) as $sponsors): ?>
+          <div class="row some-top-margin">
+              <?php foreach ($sponsors as $sponsor): ?>
+                  <div class="col3 container">
+                      <img src="<?= base_url() .'static/images/sponsors/'. $sponsor['image'] ?>" alt="<?= $sponsor['name'] ?> Logo" />
+                      <p><?= $sponsor['name'] ?></p>
+                  </div>
+              <?php endforeach; ?>
+          </div>
+      <?php endforeach; ?>
+      <h3><?= __('Year 2014 and before') ?></h3>
+      <?php foreach (array_chunk($sponsors2014, 4) as $sponsors): ?>
+          <div class="row">
+              <?php foreach ($sponsors as $sponsor): ?>
+                  <div class="col3 container">
+                      <img src="<?= base_url() .'static/images/sponsors/'. $sponsor['image'] ?>" alt="<?= $sponsor['name'] ?> Logo" />
+                      <p><?= $sponsor['name'] ?></p>
+                  </div>
+              <?php endforeach; ?>
+          </div>
+      <?php endforeach; ?>
+  </div>
     <div class="container">
         <!--<header>
             <h1>Spon<span class="tabheading">sors</span></h1>
